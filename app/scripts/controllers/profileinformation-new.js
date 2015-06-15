28ed6f2540540585ad0cff4cbb4373ff72e2e06f
Milton Ivan Terreno <miltonivanterreno@gmail.com>
2015-06-15 01:44:48 -0300

    Se corrigió el query al 'profile'(sin array), y se dejo preseleccionado el 'gender'

--- app/scripts/controllers/profileinformation-new.js
+++ app/scripts/controllers/profileinformation-new.js
@@ -11,8 +11,9 @@ angular.module('saludWebApp')
   .controller('ProfileInformationNewCtrl', function ($scope,Profile,Gender,$routeParams) {
 
         $scope.profile= new Profile();
+        $scope.profile.gender_id=1;
         var genders_data = Gender.query(function(){
-                $scope.genders = genders_data[0].data.genders;
+                $scope.genders = genders_data.resource;
         });
         $scope.addProfile = function(){
             $scope.profile.$save(function(){

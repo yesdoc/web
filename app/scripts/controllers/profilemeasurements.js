e2e850eb32171865dbe92dea60d759f347af7f85
navicor <miltonivanterreno@gmail.com>
2015-09-03 18:20:04 -0300

    grafica de barras  horizontales

--- app/scripts/controllers/profilemeasurements.js
+++ app/scripts/controllers/profilemeasurements.js
@@ -8,12 +8,33 @@
  * Controller of the saludWebApp
  */
 angular.module('saludWebApp')
-  .controller('ProfileMeasurementsCtrl', function ($scope,$location,$cookies, ProfileMeasurementsLatest) {
+  .controller('ProfileMeasurementsCtrl', function ($scope,$location,$cookies, ProfileMeasurements, ProfileMeasurementsLatest) {
         if(!$cookies.get('profile_id')){
             $location.path('/login');
         }else{
             var data = ProfileMeasurementsLatest.get({id: $cookies.get('profile_id')},function(){
                 $scope.measurements=data.resource;
             });   
+
+            $scope.data=[]
+                var d = ProfileMeasurements.get({id: $cookies.get('profile_id')},function(){
+                    var dr=d.resource
+                    var graph=[];
+                    for (var i  in dr){
+                        if(dr[i].measurement_type.id == '1'){
+                            graph[graph.length]={name:dr[i].measurement_type.name,score:dr[i].value}
+                        }
+                    }
+                    $scope.data=graph;
+                });   
+            /*
+            $scope.data = [
+                {name: "Greg", score: 98},
+                {name: "Ari", score: 96},
+                {name: 'Q', score: 75},
+                {name: "Loser", score: 48}
+            ];
+            */
         }
+
 });

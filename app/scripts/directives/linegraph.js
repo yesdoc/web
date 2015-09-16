c0bd703f8232d9bf75eff61290ea662ba19173c4
navicor <miltonivanterreno@gmail.com>
2015-09-15 23:41:20 -0300

    Fixes on linegraph

--- app/scripts/directives/linegraph.js
+++ app/scripts/directives/linegraph.js
@@ -14,18 +14,12 @@ angular.module('saludWebApp')
         // Se verifica que el servicio de inyeccion de d3 haya terminado
         d3Service.d3().then(function(d3) {
 
-         // Se evaluan los atributos de la div que contiene el svg
-          var margin = parseInt(attrs.margin) || 20,
-              barHeight = parseInt(attrs.barHeight) || 20,
-              barPadding = parseInt(attrs.barPadding) || 5;
-            
+          // Se evaluan los atributos de la div que contiene el svg
+          var margin = {top: 20, right: 20, bottom: 30, left: 40};
+ 
           // Se crea el SVG y se lo introduce en <div vgraph>
           var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%')
-            .style('height', '300px')
-	       .append("g")
-            .attr("transform", "translate(" + 30 + "," + 20 + ")");
+            .append('svg');
 
           // Se encarga de la parte responsive de la grafica
           $window.onresize = function() {
@@ -38,18 +32,33 @@ angular.module('saludWebApp')
           // hasta acá
 
 
+          var svgWidth = d3.select(ele[0])[0][0].offsetWidth,
+          svgHeight = 300 ;
+
+          svg
+            .attr('width', svgWidth)
+            .attr('height', svgHeight)
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+
           // Dibuja la gráfica
           scope.render = function(data) {
-
-            // Elimina todos los elementos en el svg
-            svg.selectAll('*').remove();
             
-
-            if (!data) return;
+            var chartWidth = svgWidth-margin.left-margin.right,
+              chartHeight = svgHeight-margin.top-margin.bottom ;
 
             // Funcion que se encarga de parsear un string con una fecha en  formato iso
             // a date
             var parseDate = d3.time.format.iso.parse;
+
+
+            // Elimina todos los elementos en el svg
+            svg.selectAll('*').remove();
+            
+            // Si no hay datos, break!
+            if (!data){ 
+              return;
+            }
 
             // Se preparan los datos que se utilizan para la gráfica
             data.forEach(function(d) {
@@ -57,51 +66,45 @@ angular.module('saludWebApp')
                 d.value = d.value;
             });
 
-            // 
-            var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-            height = scope.data.length * (barHeight + barPadding);
-
-            svg.attr('height', height);
-
 
             // se delimita el rango de valores para el eje x (la variable x es
             // una función que coloca lo pasado por parametro en una posicion
             // entre 0 y width)
             var x = d3.time.scale()
-                .rangeRound([0, width*0.95]);
+                .rangeRound([0, chartWidth]);
 
             // Se establece el dominio de datos para el eje X (se pasan los
             // elementos pertenecientes por ser ordinal)
             var xValues = data.map(function(d) { return d.date; });
             var init = new Date(xValues[0]);
             var end = new Date(xValues[xValues.length - 1]);
-            init.setDate(init.getDate()-2);
-            end.setDate(end.getDate()+2);
+            
+            init.setDate(init.getDate());
+            end.setDate(end.getDate());
             x.domain([init,end]);
 
             // se delimita el rango de valores para el eje Y ( la variable y es
             // una función que coloca lo pasado por parametro en una posicion
             // entre 0 y height)
             var y = d3.scale.linear()
-                .range([height, 0]);
+                .range([chartHeight, 0]);
 
-
-            // Propiedades del eje X
+            // Propiedades del eje X ;TODO: CAMBIAR FORMATO DE FECHA SEGUN LA
+            // DISTANCIA ENTRE LA PRIMER FECHA Y LA ULTIMA
             var xAxis = d3.svg.axis()
                 .scale(x)
-//                .tickPadding(10)
-      .tickSize(-height, -height)
-                .orient("bottom")
+                .tickPadding(10)
+                //.tickSize(-height, -height) // linea vertical
+                .orient('bottom')
                 .tickValues(xValues)
-                .tickFormat(d3.time.format("%d-%m"));
+                .tickFormat(d3.time.format('%d-%m'));
 
             // Propiedades del eje Y
             var yAxis = d3.svg.axis()
                 .scale(y)
-//      .tickSize(0, -width)
-//      .tickFormat(function(d) { return ''; })
-                .orient("left")
-                .ticks(10, "kg");
+                //.tickSize(-width, -width) // Linea horizontal
+                .orient('left')
+                .ticks(10, 'kg');
 
 
 
@@ -111,77 +114,70 @@ angular.module('saludWebApp')
 
             // Se crea un objeto <g>(shape group),se lo introduce en <svg>
             // correspondiente a la información del eje de las X
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis)
+            svg.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(0,' + (chartHeight) + ')')
+                .call(xAxis);
 
             // Se crea un objeto <g>(shape group),se lo introduce en <svg>
             // correspondiente a la información del eje de las Y
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
+            svg.append('g')
+              .attr('class', 'y axis')
+              .call(yAxis);
 
             var line = d3.svg.line()
-                .x(function(d,i) { return x(d.date);})
-                .y(function(d) {return y(d.value);});
+              .x(function(d) { return x(d.date);})
+              .y(function(d) {return y(d.value);});
 
-            var path = svg.append("path")
-                .attr("class", "graph");
+            svg.append('path')
+              .attr('class', 'graph');
 
-            svg.selectAll("circle")
-                .data(data)
-                .enter().append("circle")
-                .attr("class", "dot")
-                .attr("r", 5)
-                .attr("cx", function(d) { return x(d.date); })
-                .attr("cy", function(d) { return y(d.value); })
-                .on("mouseover", function(d) { showData(this, d);})
-                .on("mouseout", function() { hideData(this);});
+            svg.selectAll('circle')
+              .data(data)
+              .enter().append('circle')
+                .attr('class', 'dot')
+                .attr('r', 5)
+                .attr('cx', function(d) { return x(d.date); })
+                .attr('cy', function(d) { return y(d.value); })
+                .on('mouseover', function(d) { showData(this, d);})
+                .on('mouseout', function() { hideData(this);});
 
-            zoomed();
+            svg.select('.graph')
+            .attr('d', line(data));
+                
 
             function showData(dot, it) {
-                var coord = d3.mouse(dot);
-                var chartTip = d3.select(".tip");
-                
-                var l=$('#v').offset().left
+              var coord = d3.mouse(dot);
+              var chartTip = d3.select('.tip');
+
+              var l=$('#v').offset().left
                 var t=$('#v').offset().top
-                chartTip.style("left", (coord[0]+l) + "px" );
-                chartTip.style("top", (coord[1]+t-20) + "px");
-                $(".tip").html(it.value+' kg');
-                $(".tip").fadeIn(100);
-                
-                d3.select(dot)
-                    .transition()
-                    .duration(200)
-                    .attr("r", 10);
+                chartTip.style('left', (coord[0]+l) + 'px' );
+              chartTip.style('top', (coord[1]+t-20) + 'px');
+              $('.tip').html(it.value+' kg');
+              $('.tip').fadeIn(100);
+
+              d3.select(dot)
+                .transition()
+                .duration(200)
+                .attr('r', 10);
             }
             
             function hideData(dot) {
-                $(".tip").fadeOut(50);
-                
-                d3.select(dot)
-                    .transition()
-                    .duration(100)
-                    .attr("r", 5);
+              $('.tip').fadeOut(50);
+
+              d3.select(dot)
+                .transition()
+                .duration(100)
+                .attr('r', 5);
             }
 
-            function zoomed() {
-                svg.select(".x.axis").call(xAxis);
-                svg.select(".y.axis").call(yAxis);
-                svg.select(".graph")
-                .attr("d", line(data));
-                
-                svg
-                .selectAll("circle")
-                .data(data)
-                .attr("cx", function(d) { return x(d.date); })
-                .attr("cy", function(d) { return y(d.value); });
-
-            }
           };
+
         });
+
       }
+
     };
+
   }]);

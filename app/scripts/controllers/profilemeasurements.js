87c01a4c736dd0d7c8c41478b267f2564bf24fd4
navicor <miltonivanterreno@gmail.com>
2015-09-22 21:52:10 -0300

    profilemeasurments controller refactorized

--- app/scripts/controllers/profilemeasurements.js
+++ app/scripts/controllers/profilemeasurements.js
@@ -8,44 +8,77 @@
  * Controller of the saludWebApp
  */
 angular.module('saludWebApp')
-  .controller('ProfileMeasurementsCtrl', function ($scope, $location, $cookies, ProfileMeasurements, ProfileMeasurementsLatest) {
+  .controller(
+      'ProfileMeasurementsCtrl',
+      function (
+        $scope,
+        $location,
+        $cookies,
+        MeasurementType,
+        ProfileMeasurements,
+        ProfileMeasurementsLatest) {
+
     if(!$cookies.get('profile_id')){
         $location.path('/login');
     }else{
+
         // Se traen y muestran las ultimas mediciones de un perfil
-        var data = ProfileMeasurementsLatest.get({id: $cookies.get('profile_id')},function(){
-            $scope.measurements=data.resource;
+        var data = ProfileMeasurementsLatest.get(
+            {id: $cookies.get('profile_id')},
+            function(){
+          $scope.measurements=data.resource;
         });   
 
+        
+
+        
+        // ###################### Gráficas ####################################
+        
+        
         // variable que contiene los datos a mostrar por la grafica
         $scope.data=[]
 
-          var getPeso = function(){
-            /* funcion auxiliar para obtener el peso de un perfil, en particular, 
-             * en el futuro será reemplazado por un recurso
-             */
-            var graph=[];
-            var d = ProfileMeasurements.get({id: $cookies.get('profile_id')},function(){
-              var dr=d.resource
-                for (var i  in dr){
-                  //se evalua que el id de la medicion obtenida sea 1 (id del peso)
-                  if(dr[i].measurement_type.id == '1'){
-                    //cada barra horizontal tiene un peso que se guarda en
-                    //la clave "score" y un nombre que se guarda en la
-                    //clave "name".Ej {name:'peso',score'56'}
-                    graph.push({ x : +parseDate(dr[i].datetime), y : dr[i].value})
-                  }
-                }
-            });
-            return [{
-                "key" : "Peso (kg)" ,
-                "bar": true,
-                "values" : graph }];
-          }
-       // alert(nv.models.lineWithFocusChart().width( 200 ));
-        $scope.data = getPeso();
+        // mts : MeasurmentTypes
+        var mts = MeasurementType.query( function(){
 
-        var parseDate = d3.time.format.iso.parse;
+          mts = mts.resource;
+
+          var peso_id;
+
+          $.each(mts,function (i,mt){
+            if (mt.name == "Peso"){
+              peso_id = mt.id;
+            }
+          });
+
+          // función auxiliar para convertir de string a date
+          var parseDate = d3.time.format.iso.parse;
+        
+          var d = ProfileMeasurements.get(
+              {id: $cookies.get('profile_id'),type: peso_id},
+              function(){
+
+            // Lista de valores de la gráfica
+            var vList=[];
+
+            $.each(d.resource,function(i,m){
+
+              var fecha = +parseDate(m.datetime)
+              var valor = m.value
+
+              // Se agrega el par { x:fecha, y:valor }
+              vList.push({ x : fecha, y : valor})
+
+            });
+
+            $scope.data = [{
+                  "key" : "Peso (kg)" ,
+                  "bar": true,
+                  "values" : vList }];
+          });
+
+        });
+
     }
 
 });

1d5a60ed258974330ae5a52f4bd18bad3b9656f2
Yanina Morales <yanigmorales@gmail.com>
2015-09-24 12:57:37 -0300

    se crea un usuario y un perfil

--- app/scripts/controllers/profileinformation-new.js
+++ app/scripts/controllers/profileinformation-new.js
@@ -11,9 +11,10 @@ angular.module('saludWebApp')
   .controller('ProfileInformationNewCtrl', function ($scope,Profile,
               Users,Gender,$routeParams,$window) {
 
-        // Crea el perfil que se utilizará en la vista para persistir los
+        // Crea el perfil y el usuario que se utilizará en la vista para persistir los
         // atributos a partir del ng-model
         $scope.profile = new Profile();
+        $scope.user = new Users();
         $scope.profile.gender_id = 1;
 
         var genders_data = Gender.query(function(){
@@ -21,14 +22,13 @@ angular.module('saludWebApp')
         });
     
         $scope.addProfile = function(){
-            $scope.profile.$save(function(){
-                $scope.user = new Users();
-                $scope.user.profile_id = 1;
-                $scope.user.username = "robin";
-                $scope.user.email = 'batman@no.com';
-                $scope.user.password = 'batman';
+            $scope.profile.$save(function(profile_data){
+        
+                // Luego de guardar el perfil, debo traer su id
+                alert(profile_data.resource.id);
+                $scope.user.profile_id = profile_data.resource.id;
+                $scope.user.username = 'cincuentaynueve';
 
-                alert("holaaaaaaaaaa");
                 $scope.user.$save(function(){
                     alert("Saved");
                     $window.location = '/#/myProfileInformation'

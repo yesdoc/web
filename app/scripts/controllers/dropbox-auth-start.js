6fb40c73271bada352ba74cd3873f95b75329a93
navicor <miltonivanterreno@gmail.com>
2015-10-19 19:41:47 -0300

    refactor

--- app/scripts/controllers/dropbox-auth-start.js
+++ app/scripts/controllers/dropbox-auth-start.js
@@ -12,26 +12,34 @@ angular.module('saludWebApp')
 
     Auth.isLogged();
 
-    $scope.msg={'dropbox':'Conectar con mi Dropbox','drive':'Conectar con mi Google Drive'}
-    $scope.isDisabled={'dropbox':'','drive':'disabled'}
+    $scope.locations = {
+      'dropbox':{'msg':'Conectar con mi Dropbox', 'isConnected':false},
+      'drive':{'msg':'Conectar con mi Google Drive','isConnected':true} // está en true para que quede deshabilitado
+      }
     MyStorageCredentials.query(function(response){
       var credentials = response.resource;
       $.each(credentials,function(i,c){
 
         switch(c.storage_location.name.toLowerCase()){
           case 'dropbox':
-            $scope.msg['dropbox']='Dropbox ya está conectado';
-            $scope.isDisabled['dropbox'] = 'disabled'
+            $scope.locations.dropbox.msg ='Dropbox ya está conectado';
+            $scope.locations.dropbox.isConnected = true
             break;
           case 'drive':
-            $scope.msg['drive']='Google Drive ya está conectado';
+            $scope.locations.drive.msg = 'Google Drive ya está conectado';
+            $scope.locations.drive.isConnected = true
             break;
           default:
             break;
-        }
+          }
+        });
 
       });
-    });
 
+    $scope.isDisabled = function(loc){
+      if ($scope.locations[loc].isConnected ){
+        return 'disabled'
+        }
+      }
 
   });

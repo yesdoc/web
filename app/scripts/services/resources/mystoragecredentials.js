5259d787df69435904cf434f13e369b12087cbf8
navicor <miltonivanterreno@gmail.com>
2015-10-19 18:48:01 -0300

    Corrección de recurso

--- app/scripts/services/resources/mystoragecredentials.js
+++ app/scripts/services/resources/mystoragecredentials.js
@@ -11,11 +11,10 @@ angular.module('saludWebApp')
   .factory('MyStorageCredentials', function (global, $resource) {
 
     // URL of specific API resource
-    var url=global.getApiUrl()+'/my/storage_credentials/:id';
+    var url=global.getApiUrl()+'/my/storage_credentials';
 
     return $resource( url,
-        { id: '@_id' },
-        { update: { method: 'PUT' },
-          query: {method: 'GET', isArray: false }
+        {},
+        {query: {method: 'GET', isArray: false }
         });  
   });

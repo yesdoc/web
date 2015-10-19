b309dec999b5f144f1f00dac705f8d26d27be5b7
adamantike <adamantike@users.noreply.github.com>
2015-10-19 18:54:05 -0300

    Corrección en URL de recurso 'Profile'

--- app/scripts/services/resources/profile.js
+++ app/scripts/services/resources/profile.js
@@ -11,19 +11,19 @@ angular.module('saludWebApp')
   .factory('Profile', function (global, $resource) {
 
     // URL of specific API resource
-    var url = global.getApiUrl() + '/my/profile';
+    var url = global.getApiUrl() + '/profiles/:id';
 
-    return $resource( 
-        url,{ 
+    return $resource(
+        url,{
             id: '@_id'
             },{
             update: {
-                method: 'PUT' 
+                method: 'PUT'
                 },
             query: {
                 method: 'GET',
                 isArray: false
                 }
             }
-        );  
+        );
     });

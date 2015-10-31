d51a3d580befecf5f5f00c97f8178a2f9d10a04e
navicor <miltonivanterreno@gmail.com>
2015-10-31 00:16:13 -0300

    se muestran los grupos y las personas dentro de los mismos

--- app/scripts/controllers/profileinformation.js
+++ app/scripts/controllers/profileinformation.js
@@ -19,10 +19,13 @@ angular.module('saludWebApp')
       PermissionTypes,
       GroupMembershipTypes,
       MyGroups,
+      Groups,
       Auth,
       $location) {       
 
         Auth.isLogged(function(){
+
+        $scope.group = {};
 
         var dataProfile = MyProfile.get(function(){
           var profile=dataProfile.resource;
@@ -31,29 +34,41 @@ angular.module('saludWebApp')
           }); 
 
         $scope.groups = [];
-        var g_groups = MyGroups.get(function(){
-          $scope.groups = g_groups.resource;
-          });
-
-
-      var addGroupModal = $modal({ 
-        scope: $scope,
-        templateUrl: "views/addGroup.html", 
-        contentTemplate: false, 
-        html: true, 
-        show: false });
-      
-      /* Show Analysis File Modal */
-      $scope.showAddGroup = function () {
-        GroupMembershipTypes.query(function(response){
-          $scope.membership_types = response.resource;
-
-          PermissionTypes.query(function(response){
-            $scope.permission_types = response.resource;
-
-            addGroupModal.$promise.then(addGroupModal.show);
-          });
+        MyGroups.get(function(response){
+          $scope.groups = response.resource;
+          GroupsMembers.get(function(response){
+            $scope.groups.members = response.resource;
+            });
         });
+
+
+
+        var addGroupModal = $modal({ 
+          scope: $scope,
+          templateUrl: "views/addGroup.html", 
+          contentTemplate: false, 
+          html: true, 
+          show: false });
+      
+        /* Show Analysis File Modal */
+        $scope.showAddGroup = function () {
+          GroupMembershipTypes.query(function(response){
+            $scope.membership_types = response.resource;
+
+            PermissionTypes.query(function(response){
+              $scope.permission_types = response.resource;
+
+              addGroupModal.$promise.then(addGroupModal.show);
+            });
+          });
+        
+        $scope.submitGroup = function(){
+          Groups.save($scope.group ,function(response,status){
+              if (status=='200'){
+                log('Se creó el grupo');
+                }
+            });
+          }
 
       };
 

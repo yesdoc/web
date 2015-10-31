43228663fa3f88b9989e18cece84db8d24eaf7c8
navicor <miltonivanterreno@gmail.com>
2015-10-31 20:53:03 -0300

    scaffold para agregar miembros con modal

--- app/scripts/controllers/profileinformation.js
+++ app/scripts/controllers/profileinformation.js
@@ -20,6 +20,7 @@ angular.module('saludWebApp')
       GroupMembershipTypes,
       MyGroups,
       Groups,
+      GroupsMembers,
       Auth,
       $location) {       
 
@@ -36,10 +37,12 @@ angular.module('saludWebApp')
         $scope.groups = [];
         MyGroups.get(function(response){
           $scope.groups = response.resource;
-          GroupsMembers.get(function(response){
-            $scope.groups.members = response.resource;
+          $.each($scope.groups,function(i,g){
+            GroupsMembers.get({group_id:g.id},function(response){
+              g.members = response.resource;
+              });
             });
-        });
+          });
 
 
 
@@ -68,9 +71,22 @@ angular.module('saludWebApp')
                 log('Se creó el grupo');
                 }
             });
-          }
+          }//end submitGroup
+        };// end showAddGroup
 
-      };
+
+        var addMemberModal = $modal({ 
+          scope: $scope,
+          templateUrl: "views/addGroup.html", 
+          contentTemplate: false, 
+          html: true, 
+          show: false });
+      
+
+        $scope.addMember = function($index){
+          var g = $scope.groups[$index];
+          addMemberModal.$promise.then(addMemberModal.show);
+          }
 
         
 

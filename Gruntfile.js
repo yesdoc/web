1922a6e15656ec4a52a26cda7f206b68d44217e6
adamantike <adamantike@users.noreply.github.com>
2015-11-10 02:05:56 -0300

    Inclusión de archivos HTML en 'views/' para Grunt usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -232,7 +232,10 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: [
+        '<%= yeoman.dist %>/{,*/}*.html',
+        '<%= yeoman.dist %>/views/{,*/}*.html'
+      ],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       js: ['<%= yeoman.dist %>/scripts/{,*/}*.js'],
       options: {

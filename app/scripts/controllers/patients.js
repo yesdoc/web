90673b87b110d9cb6eaf227eaded103de295272e
Yanina Morales <yanigmorales@gmail.com>
2015-11-16 03:41:05 -0300

    se le muestra a los medicos los pacientes que le han compartido sus análisis

---
+++ app/scripts/controllers/patients.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saludWebApp.controller:PatientsCtrl
+ * @description
+ * # PatientsCtrl
+ * Controller of the saludWebApp
+ */
+angular.module('saludWebApp')
+  .controller(
+    'PatientsCtrl',
+    function (
+      $scope,
+      $location,
+      $cookies,
+      Auth,
+      MeasurementType,
+      MySharedProfiles,
+      MySharedAnalyses,
+      ProfileMeasurements,
+      Profile,
+      ProfileMeasurementsLatest) {
+          Auth.isLogged(function(){
+
+              Auth.getAuth(function(token){
+                  
+                  MySharedProfiles.query(function(response){
+                      $scope.patients = response.resource;
+                      $.each($scope.patients,function(i , c){
+                        Profile.get({id:c.id,element:'gravatar',size: 64},function(response){
+                          c.src = response.resource.gravatar_url;
+                        });
+                      });
+                  });
+                  
+              });
+
+          }); 
+      });

c6d8a33c1dd392a534e79ff8892637886319738e
Yanina Morales <yanigmorales@gmail.com>
2015-11-16 13:21:39 -0300

    Se añadieron las acciones asociado a los pacientes para ver los análisis compartidos.

--- app/scripts/controllers/compartidos.js
+++ app/scripts/controllers/compartidos.js
@@ -26,7 +26,9 @@ angular.module('saludWebApp')
     Auth.isLogged(function(){
 
       Auth.getAuth(function(token){
-
+        
+        
+        $scope.profile_id = $routeParams.id;
         $scope.analyses = [];
 
         MySharedProfiles.query(function(response){
@@ -34,40 +36,39 @@ angular.module('saludWebApp')
           });
 
         $scope.load = function(profile_id){
-
-        var profile_id = profile_id;
-
-        MySharedAnalyses.get({profile : profile_id},function(response){
-
-          $scope.analyses = response.resource;
-
-
-          $.each($scope.analyses,function(i,a){
-            a.datetime = a.datetime+'Z'; 
-            var q_af = Analysis.get({id:a.id,element:'files'},function(){
-              $.each(q_af.resource,function(i,af){
-                a.af_id = af.id 
-              });
-              if ( a.af_id ){
-                a.imageSrc = ( global.getApiUrl() + '/analysis_files/' + a.af_id + '/thumbnail_by_query?token='+token);
-              }
-              else{
-                a.imageSrc = 'images/escul.jpeg';
-              }
-            });
-
-            a.datetime = new Date(a.datetime)
-
-          });
-
-          if(!$scope.$$phase) {
-            $scope.apply();
-            }
-
-        });
-
+            get_analyses(profile_id);
         }
 
+        function get_analyses(profile_id){
+    
+          MySharedAnalyses.get({profile : profile_id},function(response){
+            $scope.analyses = response.resource;
+
+            $.each($scope.analyses,function(i,a){
+                a.datetime = a.datetime+'Z'; 
+                var q_af = Analysis.get({id:a.id,element:'files'},function(){
+                $.each(q_af.resource,function(i,af){
+                    a.af_id = af.id 
+                });
+                if ( a.af_id ){
+                    a.imageSrc = ( global.getApiUrl() + '/analysis_files/' + a.af_id + '/thumbnail_by_query?token='+token);
+                }
+                else{
+                    a.imageSrc = 'images/escul.jpeg';
+                }
+                });
+
+                a.datetime = new Date(a.datetime)
+
+                });
+
+                if(!$scope.$$phase) {
+                    $scope.apply();
+                    }
+
+            });
+        }
+        get_analyses($scope.profile_id);
 
       });
     });

50edf0f681b33e368ab8de4a1cfdf1ea61d1ae7a
navicor <miltonivanterreno@gmail.com>
2015-11-17 07:19:33 -0300

    Correccion de hora

--- app/scripts/controllers/profilemeasurements.js
+++ app/scripts/controllers/profilemeasurements.js
@@ -24,6 +24,9 @@ angular.module('saludWebApp')
           // Se traen y muestran las ultimas mediciones de un perfil
           var data = ProfileMeasurementsLatest.get(function(){
             $scope.measurements=data.resource;
+            $.each($scope.measurements,function(i,m){
+              m.datetime = (new Date(m.datetime+'Z'));
+            });
           });   
 
         });
